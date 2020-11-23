42df0568c4be8ea2d760ae750323f4f92d3a5737
menachem m margolis <margolismendel5728@gmail.com>
2020-11-23 23:22:29 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -17,7 +17,6 @@ def pop_with_args (dog_breeds)
  small_dogs
 end
 
-def using_shift (my_favorite_cities,im_so_over_this_city)
+def using_shift (my_favorite_cities)
   my_favorite_cities.shift(im_so_over_this_city)
-  my_favorite_cities
 end
