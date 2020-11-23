6a8bdc5b2a1fb92a44e4fc09cc1929943f7a486a
menachem m margolis <margolismendel5728@gmail.com>
2020-11-23 23:13:48 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,18 @@
+def using_push(colors_in_the_rainbow,next_color)
+  colors_in_the_rainbow.push(next_color)
+  colors_in_the_rainbow
+end
+
+def using_unshift(bouroughs_in_nyc,new_neighborhood)
+  bouroughs_in_nyc.unshift(new_neighborhood)
+  bouroughs_in_nyc
+end
+
+def using_pop(continents)
+  continents.pop
+end
+
+def pop_with_args (dog_breeds)
+  small_dogs=dog_breeds.delete[3,2]
+  small_dogs
+end
